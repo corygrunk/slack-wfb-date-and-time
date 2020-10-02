d3d871d2a732bb262e6a9a1b29af0b6c93258b41
Cory <cgrunkemeyer@slack-corp.com>
2020-10-02 13:32:34 -0600

    Removed radio buttons

--- index.js
+++ index.js
@@ -18,43 +18,12 @@ const ws = new WorkflowStep("date_and_time", {
     const blocks = [
       {
         type: "section",
-        block_id: "intro-section",
+        block_id: "intro_section",
         text: {
           type: "plain_text",
           text:
-            "This is a simple step that will output date and time as a variable. A few different formats are available. These are especially handy when writing values to a spreadsheet.",
+            "This is a simple step that will output date and time as a variable. These are especially handy when writing values to a spreadsheet.",
           emoji: true
-        }
-      },
-      {
-        "type": "divider"
-      },
-      {
-        "type": "section",
-        "text": {
-            "type": "mrkdwn",
-            "text": "*Choose a format*"
-        },
-        "accessory": {
-            "type": "radio_buttons",
-            "options": [
-                {
-                    "text": {
-                        "type": "plain_text",
-                        "text": "October 31, 2020 at 9:00 AM",
-                        "emoji": true
-                    },
-                    "value": "value-0"
-                },
-                {
-                    "text": {
-                        "type": "plain_text",
-                        "text": "2020-10-31T09:00:00+00:00",
-                        "emoji": true
-                    },
-                    "value": "value-1"
-                }
-            ]
         }
       }
     ];
@@ -74,13 +43,16 @@ const ws = new WorkflowStep("date_and_time", {
 
     await update({ outputs });
   },
-  execute: async ({ step, complete, fail,  }) => {
+  execute: async ({ step, complete, fail }) => {
+    
     try {
         let now = dayjs();
         var currentTimestamp = { date: now.format("dddd, MMMM D, YYYY - HH:mm:ss") };
+        // TODO: Get user's locale
+        // TODO: Set timestamp to user's local time
         const outputs = currentTimestamp;
-        
-      await complete({ outputs });
+        console.log(outputs);
+        await complete({ outputs });
 
     } catch (e) {
       // TODO if something went wrong, fail the step ...
